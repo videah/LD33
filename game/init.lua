aa9f2c04083f0d13419620ca2ab38ffe59ee0cc9
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-08-21 21:14:20 +0100

    Compatability boilerplate

--- game/init.lua
+++ game/init.lua
@@ -12,9 +12,13 @@ end
 
 function game.drawtop()
 
+	love.graphics.print('Hello, World!', 0, 230)
+
 end
 
 function game.drawbottom()
+
+	love.graphics.print('Hello, World!', love.graphics.getWidth() - 100, 0)
 
 end
 
--- main.lua
+++ main.lua
@@ -2,7 +2,12 @@ game = require 'game'
 
 function love.load()
 
+	love.window.setMode(400, 240 * 2)
+
 	is3DS = (love.system.getOS() == '3ds')
+
+	defaultFont = love.graphics.newFont('fonts/Vera.ttf', 13)
+	love.graphics.setFont(defaultFont)
 
 	game.load()
 
@@ -12,20 +17,53 @@ function love.update(dt)
 
 	game.update(dt)
 
+	if love.keyboard.isDown('start') then love.event.quit() end
+
 end
 
 function love.draw()
 
 	-- Draw top screen
 
-	if is3DS then love.graphics.setScreen('top') end
+	if is3DS then 
 
-	game.drawtop()
+		love.graphics.setScreen('top')
+		game.drawtop()
+
+	else
+
+		love.graphics.push()
+		love.graphics.setScissor(0, 0, 400, 240)
+
+		game.drawtop()
+
+		love.graphics.setScissor()
+		love.graphics.pop()
+
+	end
 
 	-- Draw bottom screen
 
-	if is3DS then love.graphics.setScreen('bottom') end
+	if is3DS then 
+	
+		love.graphics.setScreen('bottom')
+		game.drawbottom()
 
-	game.drawbottom()
+	else
+
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.rectangle('fill', 0, 240, 40, 240)
+		love.graphics.rectangle('fill', 360, 240, 40, 240)
+
+		love.graphics.push()
+		love.graphics.translate(40, 240)
+		love.graphics.setScissor(40, 240, 320, 240)
+
+		game.drawbottom()
+
+		love.graphics.setScissor()
+		love.graphics.pop()
+
+	end
 
 end
